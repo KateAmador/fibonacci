c210126139b14f8e4131456569816384a72f9e31
KateAmador <kateamador13@gmail.com>
2022-07-02 07:25:16 -0500

    Serie fibonacci

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 0, b = 1, c =0;
+
+        for (int i = 0; i < 10; i++){
+            if (i < 9){
+                System.out.print(a + ",");
+                c = a + b;
+                a = b;
+                b = c;
+            }else{
+                System.out.print(a);
+            }
+        }
+    }
+}
